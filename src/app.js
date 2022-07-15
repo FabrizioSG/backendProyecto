b9b5c1f6e731ae622d750f4871d39a53d2818fa6
FabrizioSG <fabriziosg95@gmail.com>
2022-07-15 14:02:44 -0600

    first commit

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const HttpError = require("./models/http-error");
+const mongoose = require("mongoose");
+var cors = require('cors');
+const app = express();
+const catsRouter = require("./routes/cats.router");
+const usersRouter = require("./routes/users.router");
+const port = 3010;
+//URL que se obtiene de mongoDB (atlas)
+const url =
+  "mongodb+srv://MainUser:Fergus2022@cluster0.oec8j.mongodb.net/?retryWrites=true&w=majority";
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Express!!!!");
+});
+
+app.use("/api/cats", catsRouter);
+app.use("/api/users", usersRouter);
+
+app.use((req, res) => {
+  const error = new HttpError("Could not find this route..", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknow error ocurred!!" });
+});
+
+mongoose
+  .connect(url)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+// nodemon
+// npm install -D nodemon
